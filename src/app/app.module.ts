4b260025e3576e98700aea5e284412206e7656fb
fuser <vnovodid@gmail.com>
2018-03-31 10:25:44 +0300

    basic upload set up with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {AppRoutingModule } from './app-routing/app-routing.module';
+
 
 import { AppComponent } from './app.component';
+import { UploadComponent } from './upload/upload.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
